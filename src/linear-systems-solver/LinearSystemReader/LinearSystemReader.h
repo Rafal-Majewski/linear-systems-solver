e6de557a6cbbc21f49e538614fbf0341f1f54b94
Rafał Majewski <goodheropl@gmail.com>
2022-05-03 14:17:53 +0200

    linear systems solver read size

--- src/linear-systems-solver/LinearSystemReader/LinearSystemReader.h
+++ src/linear-systems-solver/LinearSystemReader/LinearSystemReader.h
@@ -1,14 +1,25 @@
 #include <LinearSystem/LinearSystem.h>
+#include <LinearSystem/Size/Size.h>
+#include <iostream>
 
 
 template <typename T>
 class LinearSystemReader {
 	private:
-
+	Size readSize() const;
 	public:
-	LinearSystem<T> read();
+	LinearSystem<T> read() const;
 };
 
 template <typename T>
-LinearSystem<T> LinearSystemReader<T>::read() {
+Size LinearSystemReader<T>::readSize() const {
+	int equationsCount;
+	int variablesCount;
+	std::cin >> equationsCount >> variablesCount;
+	return Size(equationsCount, variablesCount);
+}
+
+template <typename T>
+LinearSystem<T> LinearSystemReader<T>::read() const {
+	Size size = readSize();
 }
